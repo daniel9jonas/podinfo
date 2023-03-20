979ee87818f4ebaa88705e79d24ae8a8e031d438
daniel <zalfirins@vm.bioinfocentar.com>
2023-03-20 17:31:51 +0100

    update

--- add-project-source.sh
+++ add-project-source.sh
@@ -1,5 +1,5 @@
 flux create source git podinfo \
-  --url=https://github.com/daniel9jonas/podinfo \
+  --url=ssh://git@github.com/daniel9jonas/podinfo \
   --branch=master \
   --interval=1m \
   --export > ~/zalf/eks/eks-anywhere-flux/clusters/mgmt/test-pod/source.yaml
